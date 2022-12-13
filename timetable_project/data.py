802b007969498cc38868677a79dc866909ac5182
Monica Sainer Boto <monicasainerboto@gmail.com>
2022-12-13 16:38:51 +0100

    Added function to retrieve dataframe with info about employees

--- timetable_project/data.py
+++ timetable_project/data.py
@@ -2,6 +2,7 @@ from datetime import timedelta,date
 import holidays
 import pandas as pd
 
+# Calendar Dataframe
 
 def calendar(year,country='ES',subdiv=None):
     """
@@ -33,4 +34,121 @@ def calendar(year,country='ES',subdiv=None):
         calendar['holidays']=calendar['dates'].apply(lambda x: x in es_cl_holidays).map(int)
         return calendar
     calendario = public_holidays(calendario)
+
+    def times(calendario):
+        times=['Morning','Evening','Night']
+        for i in times:
+            calendario[i] = pd.Series(0)
+        return calendario
+    calendario = times(calendario)
     return calendario
+
+
+def number_workers():
+    worker= input("how many workers do you have? >>")
+    try:
+        worker = int(worker)
+        return worker
+    except ValueError:
+        print("That's not a number!")
+
+# Employees Dataframe
+
+def df_workers(number_of_workers):
+
+    def name_workers():
+        name = input('How is your worker called >>')
+        if name.strip().isdigit():
+            print("That's not a name!")
+        else:
+            name=name.capitalize()
+            return name
+
+    def age_workers():
+        age= input("How old is your worker? >>")
+        try:
+            age = int(age)
+            return age
+        except ValueError:
+            print("That's not a number!")
+
+    def salary_workers():
+        salary = input("What's the annual salary of your worker? (In €) >>")
+        try:
+            salary = float(salary)
+            return salary
+        except ValueError:
+            print("That's not a number!")
+
+    def times_workers():
+        timer =[1,2,3,4,5,6]
+        time = input("When does your worker works? >>\n"
+                "1 for Morning\n"
+                "2 for Evening\n"
+                "3 for Night\n"
+                "4 for Morning and Evening\n"
+                "5 for Morning and Night\n"
+                "6 for Evening and Night\n")
+        try:
+            time = int(time)
+            if time in timer:
+                return time
+            else: print('Select a correct value')
+        except ValueError:
+            print("That's not a number!")
+
+    def gender_workers():
+        genders ={1:'Female',2:'Male',3:'Non-binary'}
+        gender = input("What's the gender of your worker? >>\n"
+                    "1 for Female\n"
+                    "2 for Male\n"
+                    "3 for Non-binary\n")
+        try:
+            gender = int(gender)
+            if gender in genders:
+                return genders[gender]
+            else: print('Select a correct value')
+        except ValueError:
+            print("That's not a number!")
+
+    def weekend_workers():
+        weekends ={1:'Yes',2:'No'}
+        weekend = input("Does your worker work during weekends? >>\n"
+                    "1 for No\n"
+                    "2 for Yes\n")
+        try:
+            weekend = int(weekend)
+            if weekend in weekends:
+                return weekend-1
+            else: print('Select a correct value')
+        except ValueError:
+            print("That's not a number!")
+
+    def holiday_workers():
+        holidays ={1:'Yes',2:'No'}
+        holiday = input("Does your worker work during public holidays? >>\n"
+                    "1 for No\n"
+                    "2 for Yes\n")
+        try:
+            holiday = int(holiday)
+            if holiday in holidays:
+                return holiday - 1
+            else: print('Select a correct value')
+        except ValueError:
+            print("That's not a number!")
+
+    def data_workers(number_of_workers):
+        data={}
+        for i in range(number_of_workers):
+            name = name_workers()
+            age = age_workers()
+            salary = salary_workers()
+            schedule = times_workers()
+            gender = gender_workers()
+            weekend = weekend_workers()
+            holiday = holiday_workers()
+            data[i] = {'name': name,'age': age,'salary':salary,'schedule':schedule,'gender':gender,'weekend':weekend,'holiday':holiday}
+        return data
+    df = data_workers(number_of_workers)
+    df = pd.DataFrame.from_dict(df,orient='index')
+    return df
